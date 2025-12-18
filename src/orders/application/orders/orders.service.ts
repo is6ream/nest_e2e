e1a8376a96a43174fafbc2be14a4e3200d52fc0f
is6ream <dnlils10@icloud.com>
2025-12-18 19:03:03 +0500

    registered services for users and orders

--- src/orders/application/orders/orders.service.ts
+++ src/orders/application/orders/orders.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Order } from '../../entities/order.entity';
 
 @Injectable()
-export class OrdersService {}
+export class OrdersService {
+  constructor(
+    @InjectModel(Order.name) private readonly orderModel: Model<Order>,
+  ) {}
+
+  async create(orderDto: Partial<Order>): Promise<Order> {
+    return this.orderModel.create(orderDto);
+  }
+
+  async findAll(): Promise<Order[]> {
+    return this.orderModel.find().exec();
+  }
+
+  async findOne(id: string): Promise<Order> {
+    return this.orderModel.findById(id).exec();
+  }
+
+  async update(id: string, orderDto: Partial<Order>): Promise<Order> {
+    return this.orderModel
+      .findByIdAndUpdate(id, orderDto, { new: true })
+      .exec();
+  }
+
+  async delete(id: string): Promise<Order> {
+    return this.orderModel.findByIdAndDelete(id).exec();
+  }
+}
