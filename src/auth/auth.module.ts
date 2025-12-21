c40b6ae0c391d1916684ec6f7da17aa47fbb8cc1
is6ream <dnlils10@icloud.com>
2025-12-21 20:00:42 +0500

    registered all necessary dependencies in this module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,14 +1,19 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
-import { AuthController } from '../auth/api/auth/auth.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserSchema } from '../auth/entities/user.entity';
+import { UsersService } from './application/users/users.service';
+import { UsersController } from './api/users/users.controller';
 @Module({
   imports: [
+    MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]),
     JwtModule.register({
       secret: process.env.JWT_SECRET_KEY,
       signOptions: { expiresIn: '15m' },
     }),
   ],
-  controllers: [AuthController],
-  exports: [JwtModule], // Реэкспорт JwtModule
+  providers: [UsersService],
+  controllers: [UsersController],
+  exports: [MongooseModule, JwtModule], // Реэкспорт JwtModule
 })
 export class AuthModule {}
